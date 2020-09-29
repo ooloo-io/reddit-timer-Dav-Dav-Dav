a19c68c8970ffb3308209741b34d06e6ce4093dd
Dav <davdavdav4@gmail.com>
2020-09-29 21:40:51 +0100

    Fonts imported - Bitter and Montserrat

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
+import "./Styling/styles.css";
 import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/search">Search</Link>
-      </li>
-    </ul>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/search">Search</Link>
+        </li>
+      </ul>
+      <p className="one">hello</p>
+      <p className="two">hello</p>
+    </div>
   );
 }
 
